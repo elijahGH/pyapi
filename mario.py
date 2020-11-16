1d51a127f1fd44855587e6f70668c63349efcadc
mvidena <mvidena@hotmail.com>
2020-11-16 19:56:49 +0000

    learning about list

---
+++ mario.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+print("Hello World")
+# take some input from the user
+blue= input("What is your name? ")
+
+red= "Hello "
+
+orange = "! Welcome to Python! "
+
+print(red + blue + orange)
+print(red , blue , orange)
